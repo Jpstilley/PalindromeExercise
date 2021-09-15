2d0644a01c52c4da86062efffa94e8afd59cea31
Joey Stilley <Joey.Stilley@gmail.com>
2021-09-15 15:30:36 -0500

    Completed PalindromeExercise

---
+++ PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+
+
+        public bool IsAPalindrome(string word)
+        {
+            char[] charaters = word.ToCharArray();
+            Array.Reverse(charaters);
+            string reversed = new string(charaters);
+            return word.ToLower().Equals(reversed.ToLower());
+        }
+    }
+}
---
+++ PalindromeExerciseTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PalindromeExercise;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("radar", true)]
+        [InlineData("level", true)]
+        [InlineData("Deified", true)]
+        [InlineData("madAm", true)]
+        [InlineData("reDividEr", true)]
+        [InlineData("Joey", false)]
+        [InlineData("Camel", false)]
+        [InlineData("Google", false)]
+        public void Test1(string word, bool expected)
+        {
+            //Arrange
+            var wordSmith = new WordSmith();
+
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
